d78c2a23a131d58517c048c362b9b8b34540bf8f
chumpon-asaneerat <chumponsenate@gmail.com>
2019-07-22 19:11:36 +0700

    Add Date.format extension method.

--- src/client/js/nlib.js
+++ src/client/js/nlib.js
@@ -238,8 +238,71 @@ class NNavigator {
     };
 })();
 
-// Date Extension Methods.
-(() => {})();
+// Date.format Extension Methods.
+(() => {
+    /**
+     * Date.format - The C# like DateTime.format.
+     */
+    Date.prototype.format = function (format) {
+        // Usage:
+        // let a = new Date();
+        // d.format();
+        // d.format('yyyy-MM-dd');
+        // The avaliable format:
+        //   yyyy : year (4 digits)
+        //     yy : year (2 digits)
+        //     MM : month (1-12)
+        //     dd : date (1-31)
+        //      t : pm/am
+        //     HH : hour (0-23)
+        //     hh : hour (1-12)
+        //     mm : minute (0-59)
+        //     ss : second (0-59)
+        //     ss : second (0-59)
+        //    fff : milliseconds (0-999)
+        let date = this;
+        if (!format) format = "yyyy-MM-dd HH-mm-ss.fff";
+
+        let month = date.getUTCMonth() + 1;
+        let year = date.getUTCFullYear();
+        // year.
+        if (format.indexOf("yyyy") > -1)
+            format = format.replace("yyyy", year.toString());
+        else if (format.indexOf("yy") > -1)
+            format = format.replace("yy", year.toString().substr(2, 2));
+        // month
+        format = format.replace("MM", month.toString().padL(2, "0"));            
+        // date.
+        format = format.replace("dd", date.getUTCDate().toString().padL(2, "0"));
+        // hour - am/pm.
+        let hours = date.getUTCHours();
+        if (format.indexOf("t") > -1) {
+            if (hours > 11)
+                format = format.replace("t", "pm")
+            else
+                format = format.replace("t", "am")
+        }
+        // hour.
+        if (format.indexOf("HH") > -1)
+            format = format.replace("HH", hours.toString().padL(2, "0"));
+        if (format.indexOf("hh") > -1) {
+            if (hours > 12) hours - 12;
+            if (hours == 0) hours = 12;
+            format = format.replace("hh", hours.toString().padL(2, "0"));
+        }
+        // minute.
+        if (format.indexOf("mm") > -1)
+            format = format.replace("mm", date.getUTCMinutes().toString().padL(2, "0"));
+        // second.
+        if (format.indexOf("ss") > -1)
+            format = format.replace("ss", date.getUTCSeconds().toString().padL(2, "0"));
+        // millisecond.
+        if (format.indexOf("fff") > -1) {
+            format = format.replace("fff", date.getUTCMilliseconds().toString().padL(3, "0"));
+        }
+        return format;
+    };
+})();
 
 /** NDelegate class. The .NET like delegate. */
 class NDelegate {
