26d59629f83cc490cb3a389451f7ee5840549c22
chumpon-asaneerat <chumponsenate@gmail.com>
2019-08-05 16:42:17 +0700

    Add test server in server.js

---
+++ server.js
@@ -0,0 +1,87 @@
+const path = require("path");
+
+const express = require("express");
+
+const morgan = require("morgan");
+const helmet = require("helmet");
+const bodyparser = require("body-parser");
+const cookieparser = require("cookie-parser");
+const favicon = require("serve-favicon");
+
+const app = express()
+
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.use(cookieparser("YOUR_SECURE_KEY@123"));
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+const iconpath = path.join(__dirname, "public", "favicon.ico");
+app.use(favicon(iconpath));
+
+const distPath = path.join(__dirname, 'public', 'dist');
+const distMaxAge = { maxage: '60s' };
+const dist_libs = [
+    /* jQuery */
+    { "route": "/dist/js", "path": "jquery-3.3.1" },
+    /* jQuery UI */
+    { "route": "/dist/css", "path": "jquery-ui-1.12.1" },
+    { "route": "/dist/js", "path": "jquery-ui-1.12.1" },
+    /* bootstrap 4.x */
+    { "route": "/dist/js", "path": "popperjs-1.15.0" },
+    { "route": "/dist/js", "path": "tooltipjs-1.3.2" },
+    { "route": "/dist", "path": "bootstrap-4.2.1" },
+    /* font-awesome 5.x */
+    { "route": "/dist", "path": "font-awesome-5.9.0" },
+    /* emoji-symbols */
+    { "route": "/dist/css", "path": "emoji-symbols-1.0.0" },
+    /* flag-icon-css 3.x */
+    { "route": "/dist", "path": "flag-icon-css-3.1.0" },
+    /* moment */
+    { "route": "/dist/js", "path": "moment-2.24.0" },
+    /* chartjs */
+    { "route": "/dist", "path": "chart-js-2.8.0" },
+    /* chartjs-plugin-datalabels */
+    { "route": "/dist", "path": "chart-js-plugin-datalabels-0.6.0" },
+    /* howler */
+    { "route": "/dist/js", "path": "howler-2.1.2" },
+    /* jquery-org-chart */
+    { "route": "/dist", "path": "jquery-org-chart-2.1.3" },
+    /* tabulator */
+    { "route": "/dist", "path": "tabulator-4.3.0" },
+    /* riotjs */
+    { "route": "/dist/js", "path": "riotjs-3.13.2" },
+    /* for access precompile riotjs component */
+    { "route": "/components", "path": "../../dist/component/riot" }
+];
+
+function dist_lib(app, exportRoute, localPath) {
+    console.log('publish "' + localPath + '"');
+    app.use(exportRoute, express.static(path.join(distPath, localPath), distMaxAge));
+};
+
+// dist paths.
+dist_libs.forEach(element => {
+    dist_lib(app, element.route, element.path);
+});
+
+app.get("/", (req, res) => {
+    res.status(200).send(`It's work!!!`);
+});
+
+app.get("/:file", (req, res, next) => {
+    if (req.params.file === 'index.html') {
+        res.sendFile(path.join(__dirname, req.params.file));
+    }
+    else {
+        next();
+    }
+});
+
+const info = { PORTNO: 3000, APPNAME: 'NLib Client test server'}
+
+app.listen(info.PORTNO, () => {
+    console.log(`${info.APPNAME} start at port ${info.PORTNO}`)
+})
