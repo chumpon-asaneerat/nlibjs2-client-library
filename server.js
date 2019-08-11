ae152e6deba192485180b2219450f4a1c6c28afa
chumpon-asaneerat <chumponsenate@gmail.com>
2019-08-12 06:51:18 +0700

    server.js test code for Router.

---
+++ server.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const nlib = require("./nlib/nlib");
+const WebServer = require('./nlib/nlib-express');
+
+const wsvr = new WebServer();
+
+const express = require('express');
+const commonRoute = express.Router();
+const secureRoute = express.Router();
+
+let checkSecure = (req, res, next) => {
+    console.log('secure checked.');
+    next();
+}
+
+let checkSecure2 = (req, res, next) => {
+    console.log('secure level 2 checked.');
+    next();
+}
+
+const routes = {
+    /** @type {WebServer.RequestHandler} */
+    home: (req, res, next) => {
+        let file = path.join(nlib.paths.root, 'index.html')
+        res.sendFile(file);
+    },
+    /** @type {WebServer.RequestHandler} */
+    api1: (req, res, next) => {
+        let data = { message: 'The api 1' }
+        wsvr.sendJson(req, res, data);
+    },
+    /** @type {WebServer.RequestHandler} */
+    api2: (req, res, next) => {
+        let data = { message: 'The api 2 (secure)' }
+        wsvr.sendJson(req, res, data);
+    },
+    /** @type {WebServer.RequestHandler} */
+    api3: (req, res, next) => {
+        let data = { message: 'The api 3 (secure)' }
+        wsvr.sendJson(req, res, data);
+    }
+}
+
+commonRoute.get('/api1', routes.api1)
+
+secureRoute.use(checkSecure); // all route check will the secure.
+secureRoute.get('/api2', routes.api2) // default secure
+secureRoute.get('/api3', checkSecure2, routes.api3) // default secure and custom secure
+
+wsvr.get('/', routes.home)
+// setup Router(s).
+wsvr.app.use('/api', commonRoute);
+wsvr.app.use('/api', secureRoute);
+
+wsvr.listen();
