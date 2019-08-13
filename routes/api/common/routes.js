9d66e7412dbc756d2b64336167f592b65cd97454
chumpon-asaneerat <chumponsenate@gmail.com>
2019-08-14 01:55:28 +0700

    WebServer sendFile fixed. (required to send full path).

--- routes/api/common/routes.js
+++ routes/api/common/routes.js
@@ -7,8 +7,9 @@ const rootPath = process.env['ROOT_PATHS'];
 const nlibPath = path.join(rootPath, 'nlib');
 // for nlib-server dev project
 //const nlibPath = path.join(rootPath, 'src', 'server', 'js', 'nlib');
+const nlibjs = path.join(nlibPath, 'nlib');
+const nlib = require(nlibjs);
 
-//const nlibjs = path.join(nlibPath, 'nlib');
 const nlibExprjs = path.join(nlibPath, 'nlib-express');
 
 const WebServer = require(nlibExprjs);
